8163eea0e46ac02bc750626e623950c13c1713ef
Arvin Lin <wedass8778@gmail.com>
2018-04-26 17:17:07 -0700

    Add many functions into the list lol

--- src/LinkedList/LinkedList.go
+++ src/LinkedList/LinkedList.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type LinkedList struct{
 	Header *ListNode
+	size int
 }
 
 /**
@@ -21,39 +22,129 @@ type ListNode struct {
 func NewLinkedList() *LinkedList {
 	//declare local variable header
 	header := &ListNode{ 0 , nil }
-	newList := LinkedList{ header }
+	newList := LinkedList{ header , 0 }
 	return &newList
 }
 
 /**
 *	Insert things to the list
 **/
-func ( lst LinkedList ) Insert( newElement int , curNode *ListNode ) {
+func ( lst *LinkedList ) Insert( newElement int , curNode *ListNode ) {
 	//declaration for new Node
 	newNode := ListNode{ newElement , nil }
 
 	newNode.Next = curNode.Next
 	curNode.Next = &newNode
+
+	lst.size++
 }
 
 /**
 *	Add element to the list in ascending order
+*	@param int element getting added to the list
 **/
-func ( lst LinkedList ) Add( newElement int ) {
+func ( lst *LinkedList ) Add( newElement int ) {
 	nodePtr := lst.Header
 
 	for ; nodePtr.Next != nil && nodePtr.Next.Element < newElement ;
-		 nodePtr = nodePtr.Next {
-
-	}
+		 nodePtr = nodePtr.Next { }
 
 	lst.Insert( newElement , nodePtr )
 }
 
 /**
+*	Remove element from the list
+*	@param int element getting remove from the list
+*	@return true if the element is successfully removed
+**/
+func ( lst *LinkedList ) Remove( oldElement int ) bool {
+	if lst.IsEmpty( ){
+		return false
+	}
+
+	nodePtr := lst.Header
+
+	for ; nodePtr.Next.Element != oldElement ;
+		nodePtr = nodePtr.Next {
+			if nodePtr.Next.Next == nil{
+				return false
+			}
+	}
+
+	nodePtr.Next = nodePtr.Next.Next
+	lst.size--
+
+	return true
+}
+
+/**
+*	Return the element at the given index
+*	@param int index 
+*	@return the node of the index if the node exist
+**/
+func ( lst *LinkedList ) Retrieve( index int ) *ListNode {
+	if index >= lst.size {
+		return nil 
+	}
+
+	nodePtr := lst.Header
+
+	for ; index > 0 ; index-- {	
+		nodePtr = nodePtr.Next
+	}
+
+	return nodePtr
+}
+
+/**
+*	Check if the list contains some element
+*	@param int some element to be found
+*	@return true if the item does exist in the list
+**/
+func ( lst *LinkedList ) Contains( someElement int ) bool {
+	if lst.IsEmpty( ) {
+		return false
+	}
+
+	nodePtr := lst.Header
+
+	for ; nodePtr.Next.Element != someElement ; 
+		nodePtr = nodePtr.Next { 
+			if nodePtr.Next.Next == nil {
+				return false
+			}
+	}
+
+	return true
+}
+
+/**
+*	Lazily delete the list
+**/
+func ( lst *LinkedList ) Clear( ) {
+	lst.Header.Next = nil
+	lst.size = 0
+}
+
+/**
+*	@return the size of the list
+**/
+func ( lst *LinkedList ) Size( ) int {
+	return lst.size
+}
+
+/**
+*	Check if the list is empty
+*	@return true if the list contain no element
+**/
+func ( lst *LinkedList ) IsEmpty( ) bool {
+	return lst.Header.Next == nil
+}
+
+/**
 *	Print every item in the current list
 **/
-func ( lst LinkedList ) Show() {
+func ( lst *LinkedList ) Show() {
 	for nodePtr := lst.Header.Next ; nodePtr != nil ; nodePtr = nodePtr.Next {
 		fmt.Printf( "%v " , nodePtr.Element )
 	}
--- src/main/main.go
+++ src/main/main.go
@@ -13,8 +13,15 @@ func main() {
 	for i := 0 ; i < 20 ; i++ {
 		TestList0.Add( rand.Intn(100) )
 	}
+	fmt.Println( TestList0.Remove(10) )
 
 	fmt.Println( TestList0.Header.Element == 0 )
 	fmt.Println( TestList0.Header.Next.Element )
+	fmt.Println( "Cotains 11 " , TestList0.Contains( 11 ) )
+	fmt.Println( "Size: " , TestList0.Size( ) )
+	fmt.Println( "List is Empty " , TestList0.IsEmpty( ) )
 	TestList0.Show()
+	fmt.Println( )
+	TestList0.Clear()
+	fmt.Println( "List is Empty " , TestList0.IsEmpty( ) )
 }
